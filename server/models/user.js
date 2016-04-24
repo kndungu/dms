d11f8dc3a4a40d6bc999fc1793456ebeac94a4b2
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-04-25 01:08:54 +0300

    Define the user schema

---
+++ server/models/user.js
@@ -0,0 +1,48 @@
+// Will do the heavy lifting
+var mongoose = require('mongoose');
+
+// Set up the database
+var mongoose = require('mongoose');
+var dataseUri = process.env.DATABASE_URI;
+var db = mongoose.connect(dataseUri);
+
+// Provide feedback and get connection object
+var db = mongoose.connection;
+db.on('error', function(err) {
+    console.log(err);
+});
+db.once('open', function() {
+    console.log('Successfully connected to db');
+});
+
+// To be able to autoincrement the id field
+var autoIncrement = require('mongoose-auto-increment');
+autoIncrement.initialize(db);
+
+// To create the "table"
+var Schema = mongoose.Schema;
+
+// Define a "Table"
+var UserSchema = new Schema({
+    username: String,
+    name: {
+        first: String,
+        last: String
+    },
+    email: String,
+    password: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date
+});
+
+// Create the autoincrementing id field starting at 1
+UserSchema.plugin(autoIncrement.plugin, {
+    model: 'User',
+    startAt: 1,
+    field: 'id'
+});
+
+module.exports = mongoose.model('User', UserSchema);
