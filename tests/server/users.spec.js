aa8bdba76833de1e4b4215a79f302adcfdc4e6e9
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-05-03 20:42:21 +0300

    Add user tests for the role

--- tests/server/users.spec.js
+++ tests/server/users.spec.js
@@ -9,8 +9,8 @@ describe('Users Tests', function() {
         .post('/users')
         .send({
           username: 'first',
-          firstname: 'Unique',
-          lastname: 'Name',
+          firstName: 'Unique',
+          lastName: 'Name',
           email: 'unique@name.com',
           role: 'user',
           password: 'testPass'
@@ -29,8 +29,8 @@ describe('Users Tests', function() {
         .post('/users')
         .send({
           username: 'unique',
-          firstname: 'First',
-          lastname: 'User',
+          firstName: 'First',
+          lastName: 'User',
           email: 'first@user.com',
           role: 'user',
           password: 'testPass'
@@ -41,6 +41,47 @@ describe('Users Tests', function() {
           should.equal(res.status, 409);
           res.body.success.should.be.false();
           res.body.message.should.containEql('provide another email');
+          done();
+        });
+    });
+  });
+  describe('Creates user with a valid role defined', function() {
+    it('does not create user without a defined role', function(done) {
+      request(app)
+        .post('/users')
+        .send({
+          username: 'testuser',
+          firstName: 'Test',
+          lastName: 'User',
+          email: 'test@user.com',
+          password: 'testPass'
+        })
+        .set('Accept', 'application/json')
+        .end(function(error, res) {
+          should.not.exist(error);
+          should.equal(res.status, 400);
+          res.body.success.should.be.false();
+          res.body.message.should.containEql('role must be defined');
+          done();
+        });
+    });
+    it('does not create user with invalid role', function(done) {
+      request(app)
+        .post('/users')
+        .send({
+          username: 'testuser2',
+          firstName: 'Test',
+          lastName: 'User',
+          email: 'test@user2.com',
+          role: 'newrole',
+          password: 'testPass'
+        })
+        .set('Accept', 'application/json')
+        .end(function(error, res) {
+          should.not.exist(error);
+          should.equal(res.status, 400);
+          res.body.success.should.be.false();
+          res.body.message.should.containEql('is an invalid role');
           done();
         });
     });
